eb51aa46c3772a440b2bad3965d1c3dce119b58e
Harender Soni <hsoni2010@gmail.com>
2024-01-09 20:31:01 +0530

    POM update
    manual push 9.1.24.2

--- src/main/java/org/harender/Main.java
+++ src/main/java/org/harender/Main.java
@@ -2,6 +2,9 @@ package org.harender;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Here");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
 }
