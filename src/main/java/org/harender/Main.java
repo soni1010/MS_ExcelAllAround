a62a01a29e400a2ccf55f564d00421e544af0d4c
Harender Soni <hsoni2010@gmail.com>
2024-01-09 21:31:39 +0530

    Sample Addition
    Just Addition to get data based on Column Headers inXLSX

--- src/main/java/org/harender/Main.java
+++ src/main/java/org/harender/Main.java
@@ -2,9 +2,26 @@ package org.harender;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Here");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Reading File");
+
+        String filePath = "src/main/resources/FinanceSppecial.xlsx";
+        String sheetName = "Sheet1";
+        String columnHeader = "YourColumnName";
+
+        /*
+        Column 1 - Test Case ID
+        Column 2 - Environment Type
+        Column 3 - Version
+        Column 4 - TC_Type
+        Column 5 - TC_Header
+        Column 6 - TC_Steps
+        Column 7 - Expected
+        Column 8 - Execution Output
+        Column 9 - Status
+        Column 10 - Defect ID
+        */
+
+        //ReUsableExcelMethods.readXLSX_File();
+        ReUsableExcelMethods.getColumnData(filePath,sheetName,columnHeader);
     }
 }
--- src/main/java/org/harender/ReUsableExcelMethods.java
+++ src/main/java/org/harender/ReUsableExcelMethods.java
@@ -5,13 +5,17 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ReUsableExcelMethods {
 
     public static void readXLSX_File() {
 
-        try (FileInputStream fileInputStream = new FileInputStream("path/to/your/file.xlsx") ) {
+        try (FileInputStream fileInputStream = new FileInputStream("src/main/resources/FinanceSppecial.xlsx") ) {
             Workbook workbook = new XSSFWorkbook(fileInputStream);
             Sheet sheet = workbook.getSheetAt(0); // Assuming you're working with the first sheet
 
@@ -26,5 +30,48 @@ public class ReUsableExcelMethods {
             e.printStackTrace();
         }
     }
+
+    public static List<String> getColumnData(String filePath, String sheetName, String columnHeader) {
+        List<String> columnData = new ArrayList<>();
+
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            Workbook workbook = new XSSFWorkbook(fileInputStream);
+            Sheet sheet = workbook.getSheet(sheetName);
+
+            // Find the column index based on the header
+            int columnIndex = -1;
+            Row headerRow = sheet.getRow(0);
+            Iterator<Cell> cellIterator = headerRow.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                System.out.println("Column "+Integer.sum(cell.getColumnIndex(),1) +" - "+cell.getStringCellValue());
+                if (cell.getStringCellValue().equals(columnHeader)) {
+                    columnIndex = cell.getColumnIndex();
+                    break;
+                }
+            }
+
+            // If the column header is not found, return an empty list
+            if (columnIndex == -1) {
+                System.out.println("Column header not found: " + columnHeader);
+                return columnData;
+            }
+
+            // Read data from the specified column
+            Iterator<Row> rowIterator = sheet.iterator();
+            rowIterator.next(); // Skip the header row
+            while (rowIterator.hasNext()) {
+                Row row = rowIterator.next();
+                Cell cell = row.getCell(columnIndex, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                columnData.add(cell.toString());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return columnData;
+    }
+
 
 }
